af6c6e5fa68f81cea19ad9aac9617d419fd79b83
Dmitriy <qwerty122206@gmail.com>
2017-10-25 10:10:58 -0700

    132

--- CSharpHW/1/ConsoleApp1/ConsoleApp1/Program.cs
+++ CSharpHW/1/ConsoleApp1/ConsoleApp1/Program.cs
@@ -12,6 +12,8 @@ namespace ConsoleApp1
         {
             Console.ForegroundColor = ConsoleColor.Yellow;
             Console.WriteLine("dmitrii");
+            Console.ReadKey();
+            fdssfdfdsdfs
         }
     }
 }
