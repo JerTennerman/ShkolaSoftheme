0739942e25ec003e6579547f6f015d75a7328295
JerTennerman <qwerty122206@gmail.com>
2017-11-03 01:42:45 +0200

    hw3up

--- CSharpHW/3/WpfApp1/WpfApp1/MainWindow.xaml.cs
+++ CSharpHW/3/WpfApp1/WpfApp1/MainWindow.xaml.cs
@@ -34,14 +34,14 @@ namespace WpfApp1
             Email();
             Phone();
             AddInfo();
-            if(result1.Content==null && result2.Content == null && result3.Content == null && result4.Content == null && result5.Content == null && result6.Content == null && result7.Content == null )
+            if (result1.Content == null && result2.Content == null && result3.Content == null && result4.Content == null && result5.Content == null && result6.Content == null && result7.Content == null)
             {
                 result1.Content = "new member registered";
             }
         }
         private void FirstName()
         {
-            if((fnInput.Text).Length>=255 || (fnInput.Text).Any(char.IsDigit) || fnInput.Text == "")
+            if ((fnInput.Text).Length >= 255 || (fnInput.Text).Any(char.IsDigit) || fnInput.Text == "")
             {
                 result1.Content = "incorrect first name";
             }
@@ -64,45 +64,37 @@ namespace WpfApp1
         }
         private void DOB()
         {
-            int d, m, y;
-            bool r3 = Int32.TryParse(dd.Text, out d);
-            if (r3 == true && d > 0 && d < 32)
+            bool r3 = Int32.TryParse(dd.Text, out int d);
+            if (r3  && d > 0 && d < 32)
             {
-                r3 = Int32.TryParse(mm.Text, out m);
-                if (r3 == true && m > 0 && m < 13)
+                r3 = Int32.TryParse(mm.Text, out int m);
+                if (r3 && m > 0 && m < 13)
                 {
-                    r3 = Int32.TryParse(yyyy.Text, out y);
-                    if (r3 == true && y > 1900 && y < 2018)
+                    r3 = Int32.TryParse(yyyy.Text, out int y);
+                    if (r3 && y > 1900 && y < 2018)
                     {
+                        result3.Content = null;
                     }
-                    else r3 = false;
+                    else result3.Content = "incorrect DOB";
                 }
-                else r3 = false;
+                else result3.Content = "incorrect DOB";
             }
-            else r3 = false;
-            if(!r3)
-            {
-                result3.Content = "incorrect DOB";
-            }
-            else
-            {
-                result3.Content = null;
-            }
+            else result3.Content = "incorrect DOB";
         }
         private void Gender()
         {
-            if (genderInput.Text != "male" || genderInput.Text != "female")
-            {
-                result4.Content = "incorrect gender";
-            }
-            else  
+            if (genderInput.Text == "male" || genderInput.Text == "female")
             {
                 result4.Content = null;
+            }
+            else
+            {
+                result4.Content = "incorrect gender";
             }
         }
         private void Email()
         {
-            if(!(emailInput.Text).Contains('@') || emailInput.Text.Length>=255)
+            if (!(emailInput.Text).Contains('@') || emailInput.Text.Length >= 255)
             {
                 result5.Content = "incorrect Email";
             }
@@ -113,9 +105,8 @@ namespace WpfApp1
         }
         private void Phone()
         {
-            int number;
-            bool r6 = Int32.TryParse(phoneInput.Text, out number);
-            if(!r6 || !(phoneInput.Text.Length==12))
+            bool r6 = Int64.TryParse(phoneInput.Text, out long number);
+            if (!r6 || phoneInput.Text.Length != 12)
             {
                 result6.Content = "incorrect phone number";
             }
@@ -126,7 +117,7 @@ namespace WpfApp1
         }
         private void AddInfo()
         {
-            if(!(addInfo.Text.Length<2000))
+            if (addInfo.Text.Length > 2000)
             {
                 result7.Content = "too much additional info";
             }
