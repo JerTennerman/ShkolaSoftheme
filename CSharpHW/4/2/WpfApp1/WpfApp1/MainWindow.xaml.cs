afd6c48e3f9afca8fb58837b1d87f5b44f20e4c6
JerTennerman <qwerty122206@gmail.com>
2017-11-03 10:56:05 +0200

    1

--- CSharpHW/4/2/WpfApp1/WpfApp1/MainWindow.xaml.cs
+++ CSharpHW/4/2/WpfApp1/WpfApp1/MainWindow.xaml.cs
@@ -113,251 +113,136 @@ namespace WpfApp1
 
         void ZodiacSign(int d, int m)
         {
+            var bitmapImage = new BitmapImage();
+            bitmapImage.BeginInit();
             switch (m)
             {
                 case 1:
                     if (d <= 20)
-                    {
-                        var bitmapImage = new BitmapImage();
-                        bitmapImage.BeginInit();
+                    {  
                         bitmapImage.UriSource = new Uri("https://upload.wikimedia.org/wikipedia/commons/thumb/7/76/Capricorn.svg/246px-Capricorn.svg.png"); ;
-                        bitmapImage.EndInit();
-
-                        zodImg.Source = bitmapImage;
                     }
                     else
                     {
-                        var bitmapImage = new BitmapImage();
-                        bitmapImage.BeginInit();
                         bitmapImage.UriSource = new Uri("https://upload.wikimedia.org/wikipedia/commons/thumb/2/24/Aquarius.svg/350px-Aquarius.svg.png"); ;
-                        bitmapImage.EndInit();
-
-                        zodImg.Source = bitmapImage;
                     }
                     break;
                 case 2:
                     if (d <= 19)
                     {
-                        var bitmapImage = new BitmapImage();
-                        bitmapImage.BeginInit();
                         bitmapImage.UriSource = new Uri("https://upload.wikimedia.org/wikipedia/commons/thumb/2/24/Aquarius.svg/350px-Aquarius.svg.png"); ;
-                        bitmapImage.EndInit();
-
-                        zodImg.Source = bitmapImage;
                     }
                     else
                     {
-                        var bitmapImage = new BitmapImage();
-                        bitmapImage.BeginInit();
                         bitmapImage.UriSource = new Uri("https://upload.wikimedia.org/wikipedia/commons/thumb/9/95/Pisces.svg/189px-Pisces.svg.png"); ;
-                        bitmapImage.EndInit();
-
-                        zodImg.Source = bitmapImage;
                     }
                     break;
                 case 3:
                     if (d <= 20)
                     {
-                        var bitmapImage = new BitmapImage();
-                        bitmapImage.BeginInit();
                         bitmapImage.UriSource = new Uri("https://upload.wikimedia.org/wikipedia/commons/thumb/9/95/Pisces.svg/189px-Pisces.svg.png"); ;
-                        bitmapImage.EndInit();
-
-                        zodImg.Source = bitmapImage;
                     }
                     else
                     {
-                        var bitmapImage = new BitmapImage();
-                        bitmapImage.BeginInit();
                         bitmapImage.UriSource = new Uri("https://upload.wikimedia.org/wikipedia/commons/thumb/5/5e/Aries.svg/248px-Aries.svg.png"); ;
-                        bitmapImage.EndInit();
-
-                        zodImg.Source = bitmapImage;
                     }
                     break;
                 case 4:
                     if (d <= 20)
                     {
-                        var bitmapImage = new BitmapImage();
-                        bitmapImage.BeginInit();
                         bitmapImage.UriSource = new Uri("https://upload.wikimedia.org/wikipedia/commons/thumb/5/5e/Aries.svg/248px-Aries.svg.png"); ;
-                        bitmapImage.EndInit();
-
-                        zodImg.Source = bitmapImage;
                     }
                     else
                     {
-                        var bitmapImage = new BitmapImage();
-                        bitmapImage.BeginInit();
                         bitmapImage.UriSource = new Uri("https://upload.wikimedia.org/wikipedia/commons/thumb/3/3a/Taurus.svg/225px-Taurus.svg.png"); ;
-                        bitmapImage.EndInit();
-
-                        zodImg.Source = bitmapImage;
                     }
                     break;
                 case 5:
                     if (d <= 21)
                     {
-                        var bitmapImage = new BitmapImage();
-                        bitmapImage.BeginInit();
                         bitmapImage.UriSource = new Uri("https://upload.wikimedia.org/wikipedia/commons/thumb/3/3a/Taurus.svg/225px-Taurus.svg.png"); ;
-                        bitmapImage.EndInit();
-
-                        zodImg.Source = bitmapImage;
                     }
                     else
                     {
-                        var bitmapImage = new BitmapImage();
-                        bitmapImage.BeginInit();
                         bitmapImage.UriSource = new Uri("https://upload.wikimedia.org/wikipedia/commons/thumb/1/15/Gemini.svg/229px-Gemini.svg.png"); ;
-                        bitmapImage.EndInit();
-
-                        zodImg.Source = bitmapImage;
                     }
                     break;
                 case 6:
                     if (d <= 21)
                     {
-                        var bitmapImage = new BitmapImage();
-                        bitmapImage.BeginInit();
                         bitmapImage.UriSource = new Uri("https://upload.wikimedia.org/wikipedia/commons/thumb/1/15/Gemini.svg/229px-Gemini.svg.png"); ;
-                        bitmapImage.EndInit();
-
-                        zodImg.Source = bitmapImage;
                     }
                     else
                     {
-                        var bitmapImage = new BitmapImage();
-                        bitmapImage.BeginInit();
                         bitmapImage.UriSource = new Uri("https://upload.wikimedia.org/wikipedia/commons/thumb/2/29/Cancer.svg/300px-Cancer.svg.png"); ;
-                        bitmapImage.EndInit();
-
-                        zodImg.Source = bitmapImage;
                     }
                     break;
                 case 7:
                     if (d <= 22)
                     {
-                        var bitmapImage = new BitmapImage();
-                        bitmapImage.BeginInit();
                         bitmapImage.UriSource = new Uri("https://upload.wikimedia.org/wikipedia/commons/thumb/2/29/Cancer.svg/300px-Cancer.svg.png"); ;
-                        bitmapImage.EndInit();
-
-                        zodImg.Source = bitmapImage;
                     }
                     else
                     {
-                        var bitmapImage = new BitmapImage();
-                        bitmapImage.BeginInit();
                         bitmapImage.UriSource = new Uri("https://upload.wikimedia.org/wikipedia/commons/thumb/9/99/Leo.svg/184px-Leo.svg.png"); ;
-                        bitmapImage.EndInit();
-
-                        zodImg.Source = bitmapImage;
                     }
                     break;
                 case 8:
                     if (d <= 21)
                     {
-                        var bitmapImage = new BitmapImage();
-                        bitmapImage.BeginInit();
                         bitmapImage.UriSource = new Uri("https://upload.wikimedia.org/wikipedia/commons/thumb/9/99/Leo.svg/184px-Leo.svg.png"); ;
-                        bitmapImage.EndInit();
-
-                        zodImg.Source = bitmapImage;
                     }
                     else
                     {
-                        var bitmapImage = new BitmapImage();
-                        bitmapImage.BeginInit();
                         bitmapImage.UriSource = new Uri("https://upload.wikimedia.org/wikipedia/commons/thumb/0/0c/Virgo.svg/194px-Virgo.svg.png"); ;
-                        bitmapImage.EndInit();
-
-                        zodImg.Source = bitmapImage;
                     }
                     break;
                 case 9:
                     if (d <= 23)
                     {
-                        var bitmapImage = new BitmapImage();
-                        bitmapImage.BeginInit();
                         bitmapImage.UriSource = new Uri("https://upload.wikimedia.org/wikipedia/commons/thumb/0/0c/Virgo.svg/194px-Virgo.svg.png"); ;
-                        bitmapImage.EndInit();
-
-                        zodImg.Source = bitmapImage;
                     }
                     else
                     {
-                        var bitmapImage = new BitmapImage();
-                        bitmapImage.BeginInit();
                         bitmapImage.UriSource = new Uri("https://upload.wikimedia.org/wikipedia/commons/thumb/f/f7/Libra.svg/281px-Libra.svg.png"); ;
-                        bitmapImage.EndInit();
-
-                        zodImg.Source = bitmapImage;
                     }
                     break;
                 case 10:
                     if (d <= 23)
                     {
-                        var bitmapImage = new BitmapImage();
-                        bitmapImage.BeginInit();
                         bitmapImage.UriSource = new Uri("https://upload.wikimedia.org/wikipedia/commons/thumb/f/f7/Libra.svg/281px-Libra.svg.png"); ;
-                        bitmapImage.EndInit();
-
-                        zodImg.Source = bitmapImage;
                     }
                     else
                     {
-                        var bitmapImage = new BitmapImage();
-                        bitmapImage.BeginInit();
                         bitmapImage.UriSource = new Uri("https://upload.wikimedia.org/wikipedia/commons/thumb/e/ea/Scorpio.svg/214px-Scorpio.svg.png"); ;
-                        bitmapImage.EndInit();
-
-                        zodImg.Source = bitmapImage;
                     }
                     break;
                 case 11:
                     if (d <= 22)
                     {
-                        var bitmapImage = new BitmapImage();
-                        bitmapImage.BeginInit();
                         bitmapImage.UriSource = new Uri("https://upload.wikimedia.org/wikipedia/commons/thumb/e/ea/Scorpio.svg/214px-Scorpio.svg.png"); ;
-                        bitmapImage.EndInit();
-
-                        zodImg.Source = bitmapImage;
                     }
                     else
                     {
-                        var bitmapImage = new BitmapImage();
-                        bitmapImage.BeginInit();
                         bitmapImage.UriSource = new Uri("https://upload.wikimedia.org/wikipedia/commons/thumb/8/80/Sagittarius.svg/235px-Sagittarius.svg.png"); ;
-                        bitmapImage.EndInit();
-
-                        zodImg.Source = bitmapImage;
                     }
                     break;
                 case 12:
                     if (d <= 22)
                     {
-                        var bitmapImage = new BitmapImage();
-                        bitmapImage.BeginInit();
                         bitmapImage.UriSource = new Uri("https://upload.wikimedia.org/wikipedia/commons/thumb/8/80/Sagittarius.svg/235px-Sagittarius.svg.png"); ;
-                        bitmapImage.EndInit();
-
-                        zodImg.Source = bitmapImage;
                     }
                     else
                     {
-                        var bitmapImage = new BitmapImage();
-                        bitmapImage.BeginInit();
                         bitmapImage.UriSource = new Uri("https://upload.wikimedia.org/wikipedia/commons/thumb/7/76/Capricorn.svg/246px-Capricorn.svg.png"); ;
-                        bitmapImage.EndInit();
-
-                        zodImg.Source = bitmapImage;
                     }
                     break;
                 default: break;
 
             }
+            bitmapImage.EndInit();
+
+            zodImg.Source = bitmapImage;
         }
         }
 }
