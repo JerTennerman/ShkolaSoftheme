f32b3a873c91af9326c646f1585ef1e4d6c04103
JerTennerman <qwerty122206@gmail.com>
2017-11-05 12:23:57 +0200

    hw5

---
+++ CSharpHW/5/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string option;
+            do
+            {
+                Console.WriteLine("Choose option T-Triangle,S-Square,R-Romb,Q-exit");
+                option = Console.ReadLine();
+                //if (option == "q" || option == "Q")
+                //{
+                //    return;
+                //}
+                if(!(option.Equals("q", StringComparison.InvariantCultureIgnoreCase)) &&( option.Equals("t", StringComparison.InvariantCultureIgnoreCase) || option.Equals("s", StringComparison.InvariantCultureIgnoreCase) || option.Equals("r", StringComparison.InvariantCultureIgnoreCase)))
+                {
+                    Console.WriteLine("Choose length (1-10)");
+                    int n;
+                        if (Int32.TryParse(Console.ReadLine(), out n) &&( n >= 1 && n <= 10))
+                        {
+                             if (option.Equals("t", StringComparison.InvariantCultureIgnoreCase))
+                             {
+                                 buildTriangle(n);
+                             }
+                             else if (option.Equals("s", StringComparison.InvariantCultureIgnoreCase))
+                             {
+                                buildSquare(n);
+                              }
+                             else if (option.Equals("r", StringComparison.InvariantCultureIgnoreCase))
+                             {
+                                buildRomb(n);
+                             }
+                        }
+                        else
+                        {
+                            Console.WriteLine("incorrect length");
+                        }
+                }
+
+            } while (!option.Equals("q", StringComparison.InvariantCultureIgnoreCase));
+            Console.ReadKey();
+        }
+
+        static void buildTriangle(int N)
+        {
+            var str = "*";
+            for (int i = 0; i < N; i++)
+            {
+                Console.WriteLine(str);
+                str += "*";
+            }
+        }
+        static void buildSquare(int N)
+        {
+            for (int i = 0; i < N; i++)
+            {
+                for (int j = 0; j < N; j++)
+                {
+                    Console.Write("*");
+                }
+                Console.WriteLine();
+            }
+
+        }
+        static void buildRomb(int N)
+        {
+            //string[] str;
+            //str = new string[N];
+            //str[N / 2] = "*";
+            //for (int i = 0; i < N; i++)
+            //{
+            //    if(i<N/2)
+            //    {
+            //        Console.Write(str);
+            //        str[N / 2 - i] = "*";
+            //        str[N / 2 + i] = "*";
+            //    }
+            //    else
+            //    {
+            //        //Console.Write(str);
+            //        //str[N / 2 - i] = " ";
+            //        //str[N / 2 + i] = " ";
+            //    }
+            //    Console.WriteLine();
+            //}
+            for (int i = 0; i < N; i++)
+            {
+                for (int j = 0; j < N-i; j++)
+                {
+                    Console.Write(" ");
+                }
+                for (int k = 0; k < i; k++)
+                {
+                    Console.Write("* ");
+                }
+                Console.WriteLine();
+            }
+            for (int i = 0; i < N ; i++)
+            {
+                for(int j = 0; j < i; j++)
+                {
+                    Console.Write(" ");
+                }
+                for(int k = N; k > i; k--)
+                {
+                    Console.Write("* ");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
