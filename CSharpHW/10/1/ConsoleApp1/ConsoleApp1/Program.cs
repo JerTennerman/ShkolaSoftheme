6016f1a8c57194ea7e66435b37407268bab13c63
JerTennerman <qwerty122206@gmail.com>
2017-11-13 13:21:30 +0200

    hw10-1-2

---
+++ CSharpHW/10/1/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const int raz = 1001;
+            int[] arr = new int[raz+1];
+            var single = 9999;
+            Random rnd = new Random();
+            var pos = rnd.Next(raz+1);
+            arr[pos] = single;
+            var j = raz - 1;
+            var z = 0;
+            for (int i = 0; i < j; i++)
+            {
+                if(i==pos || j==pos)
+                {
+                    if(i==pos)
+                    {
+                        i++;
+                    }
+                    else
+                    {
+                        j--;
+                    }
+                }
+                arr[i] = z;
+                arr[j] = z;
+                j--;
+                z++;
+            }
+            foreach (int el in arr)
+            {
+                Console.Write(el + " ");
+            }
+            var singlePos = Search(arr, raz);
+            if (singlePos != -1)
+            {
+                Console.WriteLine("position is - " + singlePos);
+            }
+            else Console.WriteLine("each array membr got a duplicate");
+        }
+        static int Search(int[] mas, int bound)
+        {
+            var j = bound-1;
+            for (int i = 0; i < j; i++)
+            {
+                if(mas[i]!= mas[j])
+                {
+                    if (mas[i++] == mas[j])
+                    {
+                        return i--;
+                    }
+                    else return j;
+                }
+                j--;
+            }
+            return -1;
+        }
+    }
+}
---
+++ CSharpHW/10/2/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random rand = new Random();
+            const int col = 10;
+            int[] arr = new int[col];
+            Console.WriteLine("unsorted");
+            for (int i = 0; i < col; i++)
+            {
+                arr[i] = rand.Next(10);
+                Console.Write(arr[i] + " ");
+            }
+            for (int i = 0; i < col; i++)
+            {
+                for (int j = 0; j < col - i - 1; j++)
+                {
+                    if (arr[j] > arr[j + 1])
+                    {
+                        var temp = arr[j];
+                        arr[j] = arr[j + 1];
+                        arr[j + 1] = temp;
+                    }
+                }
+            }
+            Console.WriteLine("\nsorted");
+            foreach (int el in arr)
+            {
+                Console.Write(el + " ");
+            }
+        }
+    }
+}
