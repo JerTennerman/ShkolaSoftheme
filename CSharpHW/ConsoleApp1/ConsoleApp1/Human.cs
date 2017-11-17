9afe228b10750fb2eb148f94fb0de4d278107334
Dmitriy <qwerty122206@gmail.com>
2017-11-17 08:58:15 -0800

    no message

---
+++ CSharpHW/ConsoleApp1/ConsoleApp1/Human.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+   public abstract class Human :IHuman
+    {
+        public int age { get; set; }
+        public string Name { get; set; }
+        public virtual void Move()
+        {
+            Console.Write("go");
+        }
+
+        public virtual void Do()
+        {
+            Console.WriteLine("doing");
+        }
+        public virtual void Think()
+        {
+            Console.WriteLine("thinking");
+        }
+    }
+}
